48108c4eb56ccb447d66c13099f9d20cbb7a68d6
Ioana Berdei <andreea.ioanaa98@gmail.com>
2022-11-14 21:14:08 +0200

    add middleware example

--- lab8/express-app/routes/movies.js
+++ lab8/express-app/routes/movies.js
@@ -2,8 +2,17 @@ const express = require("express");
 const router = express.Router();
 const moviesController = require("../controllers/movies");
 
+// middleware check if movieId is a number
+const checkMovieId = (req, res, next) => {
+    if(req.params.movieId && isNaN(req.params.movieId)) {
+        res.status(400).json({message: "Movie id should be a number"});
+    } else {
+        next();
+    }
+}
+
 router.post("/", moviesController.createMovie);
-router.get("/:movieId", moviesController.getMovieById);
+router.get("/:movieId", checkMovieId, moviesController.getMovieById);
 router.get("/", moviesController.getAllMovies);
 router.put("/:movieId", moviesController.updateMovie);
 router.delete("/:movieId", moviesController.deleteMovie);
