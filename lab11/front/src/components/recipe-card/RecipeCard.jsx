c1ecfc5fdcf379f8584a9267edbccf38276b7e1f
Ioana Berdei <andreea.ioanaa98@gmail.com>
2022-12-13 15:09:20 +0200

    current lab progress

--- lab11/front/src/components/recipe-card/RecipeCard.jsx
+++ lab11/front/src/components/recipe-card/RecipeCard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useState } from "react";
 
 import './RecipeCard.css';
 
 const RecipeCard = ({name, prepTime, ingredients, difficulty}) => {
+    const [completedTimesCount, setCompletedTimesCount] = useState(0);
+
     return (
         <div className="recipe-card">
             <h1>{name}</h1>
@@ -17,7 +20,8 @@ const RecipeCard = ({name, prepTime, ingredients, difficulty}) => {
             <p>{`${prepTime} minutes`}</p>
             <h3>Difficulty:</h3>
             <p>{difficulty}</p>
-            <button className="recipe-details-btn">Recipe completed?</button>
+            <p>{`You completed this recipe ${completedTimesCount} times`}</p>
+            <button className="recipe-details-btn" onClick={() => setCompletedTimesCount(completedTimesCount + 1)}>Recipe completed?</button>
         </div>
     );
 };
